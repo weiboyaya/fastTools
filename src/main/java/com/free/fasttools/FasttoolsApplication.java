34a24c0878023d3e1683748c464ff39f750156f0
weiboyaya <759047731@qq.com>
2018-11-12 22:34:13 +0800

    fastTools 框架搭建

---
+++ src/main/java/com/free/fasttools/FasttoolsApplication.java
@@ -0,0 +1,12 @@
+package com.free.fasttools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasttoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FasttoolsApplication.class, args);
+    }
+}
