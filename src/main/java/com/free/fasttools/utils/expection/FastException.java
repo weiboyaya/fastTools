00f7e1ad7b29e72bfaa8d4fd53131c5e9f8671eb
weiboyaya <759047731@qq.com>
2018-11-28 17:07:15 +0800

    fastTools 添加工具类

---
+++ src/main/java/com/free/fasttools/utils/expection/FastException.java
@@ -0,0 +1,20 @@
+package com.free.utils.expection;
+
+/**
+ * @Auther: zhengwei
+ * @Date: 2018-11-28 17:04
+ * @Description:自定义异常类
+ */
+public class FastException extends  Exception{
+
+    private String errMsg="";
+
+    public FastException(String errMsg){
+        super(errMsg);
+        this.errMsg=errMsg;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+}
